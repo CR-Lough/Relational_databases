ea60f9a8481c03c145c486a72a4944ba3642166a
Connor Lough <crlough@outlook.com>
2022-05-04 19:01:01 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,194 @@
+'''
+main driver for a simple social network project
+'''
+
+import users, user_status
+from csv import DictReader
+import pandas as pd
+    
+def init_user_collection():
+    '''
+    Creates and returns a new instance of UserCollection
+    '''
+    user = users.UserCollection()
+    return user
+
+
+
+def init_status_collection():
+    '''
+    Creates and returns a new instance of UserStatusCollection
+    '''
+    status = user_status.UserStatusCollection()
+    return status
+
+def load_users(filename, user_collection):
+    '''
+    Opens a CSV file with user data and
+    adds it to an existing instance of
+    UserCollection
+
+    Requirements:
+    - If a user_id already exists, it
+    will ignore it and continue to the
+    next.
+    - Returns False if there are any errors
+    (such as empty fields in the source CSV file)
+    - Otherwise, it returns True.
+    '''
+    try:
+        with open(filename, mode ='r') as file: 
+            csv_dict_reader = DictReader(file) 
+            for row in csv_dict_reader:
+                user_collection.add_user(row['USER_ID'],row['EMAIL'],row['NAME'],row['LASTNAME'])
+    except FileNotFoundError:
+        print('Error! File not found')
+    return True
+
+def save_users(filename, user_collection):
+    '''
+    Saves all users in user_collection into
+    a CSV file
+
+    Requirements:
+    - If there is an existing file, it will
+    overwrite it.
+    - Returns False if there are any errors
+    (such as an invalid filename).
+    - Otherwise, it returns True.
+    '''
+    list_of_dicts = []
+    for i in range(len(user_collection.database)):
+        row = {
+            'USER_ID':list(user_collection.database.values())[i].user_id,
+            'EMAIL':list(user_collection.database.values())[i].email,
+            'NAME':list(user_collection.database.values())[i].user_name,
+            'LASTNAME':list(user_collection.database.values())[i].user_last_name
+        }
+        list_of_dicts.append(row)
+    df = pd.DataFrame(list_of_dicts)
+    df.to_csv(filename, index=False)
+    
+def load_status_updates(filename, status_collection):
+    '''
+    Opens a CSV file with status data and adds it to an existing
+    instance of UserStatusCollection
+
+    Requirements:
+    - If a status_id already exists, it will ignore it and continue to
+      the next.
+    - Returns False if there are any errors(such as empty fields in the
+      source CSV file)
+    - Otherwise, it returns True.
+    '''
+    pass
+
+
+def save_status_updates(filename, status_collection):
+    '''
+    Saves all statuses in status_collection into a CSV file
+
+    Requirements:
+    - If there is an existing file, it will overwrite it.
+    - Returns False if there are any errors(such an invalid filename).
+    - Otherwise, it returns True.
+    '''
+    pass
+
+
+def add_user(user_id, email, user_name, user_last_name, user_collection):
+    '''
+    Creates a new instance of User and stores it in user_collection
+    (which is an instance of UserCollection)
+
+    Requirements:
+    - user_id cannot already exist in user_collection.
+    - Returns False if there are any errors (for example, if
+      user_collection.add_user() returns False).
+    - Otherwise, it returns True.
+    '''
+    new_user = user_collection.add_user(user_id,email,user_name,user_last_name)
+    return new_user
+
+
+def update_user(user_id, email, user_name, user_last_name, user_collection):
+    '''
+    Updates the values of an existing user
+
+    Requirements:
+    - Returns False if there any errors.
+    - Otherwise, it returns True.
+    '''
+    updated_user = user_collection.modify_user(user_id, email, user_name, user_last_name)
+    return updated_user
+
+
+def delete_user(user_id, user_collection):
+    '''
+    Deletes a user from user_collection.
+
+    Requirements:
+    - Returns False if there are any errors (such as user_id not found)
+    - Otherwise, it returns True.
+    '''
+    user_collection.delete_user(user_id)
+
+
+def search_user(user_id, user_collection):
+    '''
+    Searches for a user in user_collection(which is an instance of
+    UserCollection).
+
+    Requirements:
+    - If the user is found, returns the corresponding User instance.
+    - Otherwise, it returns None.
+    '''
+    pass
+
+
+def add_status(user_id, status_id, status_text, status_collection):
+    '''
+    Creates a new instance of UserStatus and stores it in
+    user_collection(which is an instance of UserStatusCollection)
+
+    Requirements:
+    - status_id cannot already exist in user_collection.
+    - Returns False if there are any errors (for example, if
+      user_collection.add_status() returns False).
+    - Otherwise, it returns True.
+    '''
+    pass
+
+
+def update_status(status_id, user_id, status_text, status_collection):
+    '''
+    Updates the values of an existing status_id
+
+    Requirements:
+    - Returns False if there any errors.
+    - Otherwise, it returns True.
+    '''
+    pass
+
+
+def delete_status(status_id, status_collection):
+    '''
+    Deletes a status_id from user_collection.
+
+    Requirements:
+    - Returns False if there are any errors (such as status_id not found)
+    - Otherwise, it returns True.
+    '''
+    pass
+
+
+def search_status(status_id, status_collection):
+    '''
+    Searches for a status in status_collection
+
+    Requirements:
+    - If the status is found, returns the corresponding
+    UserStatus instance.
+    - Otherwise, it returns None.
+    '''
+    pass
